5c45111846a96e2688498570a8cca74d4ca2e5ac
ingestron <ingestron@otrera.nz>
2022-05-06 19:59:28 +1200

    updates

--- azure/sql-metadata@v1.sql
+++ azure/sql-metadata@v1.sql
@@ -2,19 +2,36 @@
 author: 🔀ingestron.io 
 script: azure/sql-metadata@v1
 description: |
-  Retrieve Azure SQL object metadata by schema and object name.
+  Retrieve table/view metadata by schema and object name.
   -- @schema {nvarchar} schema name.
   -- @objectName {nvarchar} object (table/view) name.
   returns {nvarchar} metadata object in JSON format.
+container: ${options.container}
+basePath: ${options.basePath}
+relativePath: ${namespace}/tables
+fileName: ${name}.md
+fileType: md
 ---
-SELECT 
-  physicalName = name,
-  type = TYPE_NAME(system_type_id),
-  logicalName = name,
-  name = name,
-  physicalType = TYPE_NAME(system_type_id),
-  precision = IIF(precision = 0, max_length, precision),
-  scale = IIF(scale = 0, max_length, scale)
-FROM sys.columns
-WHERE object_id = OBJECT_ID('[{{schema}}].[{{objectName}}]')
-FOR JSON PATH, ROOT ('structure')
+DECLARE @json NVARCHAR(MAX) = (
+  SELECT 
+    [objectId] = o.object_id,
+    [objectName] = o.name,
+    [id] = c.column_id,
+    [name] = c.name,
+    [physicalName] = c.name,
+    [logicalName] = c.name,
+    [type] = TYPE_NAME(c.system_type_id),
+    [physicalType] = TYPE_NAME(c.system_type_id),
+    [precision] = IIF(c.precision = 0, c.max_length, c.precision),
+    [scale] = IIF(c.scale = 0, c.max_length, c.scale),
+    [isNullable] = c.is_nullable,
+    [definition] = dc.definition
+  FROM sys.columns c
+  INNER JOIN sys.objects o ON o.object_id = c.object_id
+  INNER JOIN sys.types t ON c.user_type_id = t.user_type_id
+  LEFT OUTER JOIN sys.default_constraints dc ON c.default_object_id = dc.object_id
+  WHERE o.object_id = OBJECT_ID('[{{schema}}].[{{objectName}}]')
+  FOR JSON PATH, ROOT ('structure') 
+)
+
+SELECT @json as value
